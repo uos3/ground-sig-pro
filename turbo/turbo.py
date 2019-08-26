baaff69832509b52ab047508f262b98a0a83890e
Matt Brejza <mbrejza@gmail.com>
2019-08-26 18:26:15 +0100

    done encoder chain/core and decoder chain (but not core)

---
+++ turbo/turbo.py
@@ -0,0 +1,230 @@
+
+def get_allowed_k_values():
+    out = []
+    out += range(40,8,512+1)
+    out += range(512,16,1024+1)
+    out += range(1024,32,2048+1)
+    out += range(2048,64,6144+1)
+
+def get_k_index(k):
+    allowed = get_allowed_k_values()
+    return allowed.index(k)
+
+def get_f1_f2(k_index):
+    # from 5.1.3.2.3
+    f1_list = [3, 7, 19, 7, 7, 11, 5, 11, 7, 41, 103, 15, 9, 17, 9, 21, 101, 21, 57, 23, 13, 27, 11, 27, 85, 29, 33, 15, 17, 33, 103, 19, 19, 37, 19, 21, 21, 115, 193, 21, 133, 81, 45, 23, 243, 151, 155, \
+               25, 51, 47, 91, 29, 29, 247, 29, 89, 91, 157, 55, 31, 17, 35, 227, 65, 19, 37, 41, 39, 185, 43, 21, 155, 79, 139, 23, 217, 25, 17, 127, 25, 239, 17, 137, 215, 29, 15, 147, 29, 59, 65, 55, 31, 17, 171, \
+               67, 35, 19, 39, 19, 199, 21, 211, 21, 43, 149, 45, 49, 71, 13, 17, 25, 183, 55, 127, 27, 29, 29, 57, 45, 31, 59, 185, 113, 31, 17, 171, 209, 253, 367, 265, 181, 39, 27, 127, 143, 43, 29, 45, 157, 47, 13, \
+               111, 443, 51, 51, 451, 257, 57, 313, 271, 179, 331, 363, 375, 127, 31, 33, 43, 33, 477, 35, 233, 357, 337, 37, 71, 71, 37, 39, 127, 39, 39, 31, 113, 41, 251, 43, 21, 43, 45, 45, 161, 89, 323, 47, 23, 47, 263]
+    f2_list = [10, 12, 42, 16, 18, 20, 22, 24, 26, 84, 90, 32, 34, 108, 38, 120, 84, 44, 46, 48, 50, 52, 36, 56, 58, 60, 62, 32, 198, 68, 210, 36, 74, 76, 78, 120, 82, 84, 86, 44, 90, 46, 94, 48, 98, 40, 102, \
+               52, 106, 72, 110, 168, 114, 58, 118, 180, 122, 62, 84, 64, 66, 68, 420, 96, 74, 76, 234, 80, 82, 252, 86, 44, 120, 92, 94, 48, 98, 80, 102, 52, 106, 48, 110, 112, 114, 58, 118, 60, 122, 124, 84, 64, 66, 204, \
+               140, 72, 74, 76, 78, 240, 82, 252, 86, 88, 60, 92, 846, 48, 28, 80, 102, 104, 954, 96, 110, 112, 114, 116, 354, 120, 610, 124, 420, 64, 66, 136, 420, 216, 444, 456, 468, 80, 164, 504, 172, 88, 300, 92, 188, 96, 28, \
+               240, 204, 104, 212, 192, 220, 336, 228, 232, 236, 120, 244, 248, 168, 64, 130, 264, 134, 408, 138, 280, 142, 480, 146, 444, 120, 152, 462, 234, 158, 80, 96, 902, 166, 336, 170, 86, 174, 176, 178, 120, 182, 184, 186, 94, 190, 480]
+             
+    return (f1_list[k_index], f2_list[k_index])
+    
+
+def interleave(a):
+    # from 5.1.3.2.3
+    
+    k_index = get_k_index(k)
+    # pre-allocate the output
+    k = len(a)
+    b = [0]*k
+    
+    # initialise the QPP interleaver address generator
+    (f1,f2) = get_f1_f2(k_index)
+    
+    g = f1+f2
+    f = 0
+    k = 0
+    
+    for i in range(0, k):
+        b[f] = a[i]
+        
+        # increment the interleaver address generator
+        f = f + g
+        g = g + f2
+        if g >= k:
+            g = g - k
+        if f >= k:
+            f = f - k
+    
+    return b
+
+def component_encoder(c):
+    state = 0
+    k = len(c)
+    z = [0]*k
+    for i in range(0,k):
+        state_nxt = state << 1
+        if c[i]:
+            state_nxt |= ((state >> 1) & 1) ^ ((state >> 2) & 1) ^ 1;
+         else:
+            state_nxt |= ((state >> 1) & 1) ^ ((state >> 2) & 1);
+        if ((state_nxt & 1) ^ (state & 1) ^ ((state >> 2) & 1))
+            z[i] = 1
+        state = state_nxt
+    
+    # get the termination
+    if  state&7 == 0:
+        x_term = [0,0,0]
+        z_term = [0,0,0]
+    elif state&7 == 1:
+        x_term = [0,1,1]
+        z_term = [1,0,1]
+    elif state&7 == 2:
+        x_term = [1,1,0]
+        z_term = [0,1,0]
+    elif state&7 == 3:
+        x_term = [1,0,1]
+        z_term = [1,1,1]
+    elif state&7 == 4:
+        x_term = [1,0,0]
+        z_term = [1,0,0]
+    elif state&7 == 5:
+        x_term = [1,1,1]
+        z_term = [0,0,1]
+    elif state&7 == 6:
+        x_term = [0,1,0]
+        z_term = [1,1,0]
+    else: #state&7 == 7:
+        x_term = [0,0,1]
+        z_term = [0,1,1]
+        
+    return (z, x_term, z_term)
+
+def subblock_interleave(d, d_select):
+    D = len(d)
+    C = 32
+    R = math.ceil(D,C)
+    N_D = R*C-D  # number of prepended nulls
+    y = [[-1]*N_D,  d]
+    P =  [0, 16, 8, 24, 4, 20, 12, 28, 2, 18, 10, 26, 6, 22, 14, 30, 1, 17, 9, 25, 5, 21, 13, 29, 3, 19, 11, 27, 7, 23, 15, 31]
+
+    v = [0]*(R*C)
+    
+    if not d_select == 2:
+        # Perform interleaving for d_0, d_1
+        # Read out column wise, jumping between columns based on the pattern P
+        k = 0
+        for c in range(0, C):
+            col = P[c]
+            for r in range(0, R):
+                v[k] = y[ r*C + col ]
+                k += 1
+    else:
+        # For d_2, just the equation
+        K = R*C
+        for k in range(0, R*C):
+            pi_k = ( P[math.floor(k/R)] + C*(k%R) + 1 )%K
+            v[k] = y[pi_k]
+
+def pick_k(A):
+    K_list = get_allowed_k_values()
+    K = -1
+    for k in range(0, K_list):
+        if A <= K_list[k]:
+            K = K_list[k]
+            break
+    if (K < 0):
+        # error
+        return None
+    return K
+
+def bit_collection(v_0, v_1, v_2)
+    # 5.1.4.1.2 bit collection
+    w = v_0
+    for i in range(0, len(v_1)):
+        w += [v_1, v_2]
+    return w
+
+def remove_nulls(a)
+    b = []
+    for i in range(0, len(a)):
+        if not a[i] is None:
+            b += a[i]
+    return b
+    
+def encoder_core(c_bits, F):
+
+    k = len(c_bits)
+    
+    c_interleaved = interleave(c_bits)
+    
+    (z, x_term, z_term) = component_encoder(c)
+    (z_prime, x_prime_term, z_prime_term) = component_encoder(c_interleaved)
+    
+    # 5.1.3.2.1 locate the bits in the correct place on the three streams
+    d_0 = c
+    d_1 = z
+    d_2 = z_prime
+    
+    for f in range(0, F):
+        d_0(f) = None
+        d_1(f) = None
+        
+    # as in 5.1.3.2.2
+    d_0 += [x_term[0], z_term[1], x_prime_term[0], z_prime_term[1]]
+    d_1 += [z_term[0], x_term[2], z_prime_term[0], x_prime_term[2]]
+    d_2 += [x_term[1], z_term[2], x_prime_term[1], z_prime_term[2]]
+    
+    return (d_0, d_1, d_2)
+    
+
+def codeblock_encoder_chain(a_bits):
+    A = len(a_bits)
+    K = pick_k(A)
+    F = K-A
+    c = [ [0]*F, a_bits]
+    
+    (d_0, d_1, d_2) = encoder_core(c, F)
+    
+    v_0 = subblock_interleave(d_0, 0)
+    v_1 = subblock_interleave(d_1, 1)
+    v_2 = subblock_interleave(d_2, 2)
+    
+    w = bit_collection(v_0, v_1, v_2)
+        
+    # for ratematching, just remove the NULLs. (Assumes k_0 = 0)
+    # The calling function can select a sub-set of bits if it wishes
+    # e should have a length of 3K+12-2F
+    e = remove_nulls(w)
+            
+    return e
+
+def codeblock_decoder_chain(e_llrs, A)
+    K = pick_k(A)
+    F = K-A
+    
+    # This jumps over bit collection and subblock interleaving
+    #  in one step, by using one interleaver pattern
+    indicies_d_0 = range(0,K+4)
+    indicies_d_1 = range(0,K+4)+10000
+    indicies_d_2 = range(0,K+4)+20000
+    for f in range(0, F):
+        indicies_d_0(f) = None
+        indicies_d_1(f) = None
+    indicies_v_0 = subblock_interleave(indicies_d_0, 0)
+    indicies_v_1 = subblock_interleave(indicies_d_1, 1)
+    indicies_v_2 = subblock_interleave(indicies_d_2, 2)
+    indicies_w = bit_collection(indicies_v_0, indicies_v_1, indicies_v_2)
+    indicies_e = remove_nulls(indicies_w)
+    
+    # Now use the indicies to jump from e to d
+    d_0 = [0]*(K+4)
+    d_1 = [0]*(K+4)
+    d_2 = [0]*(K+4)
+    for i in range(0 ,len(e_llrs):
+        index = indicies_e[i]
+        if index < 10000:
+            d_0[index] = e_llrs[i] # (change to += if repetition is ever supported)
+        elif index < 20000:
+            d_1[index-10000] = e_llrs[i]
+        else:
+            d_2[index-20000] = e_llrs[i]
+            
+    (c_hat, crc_pass) = decoder_core(d_0, d_1, d_2)
+    
+    a_hat = c_hat[F:K-1]
+    return a_hat
